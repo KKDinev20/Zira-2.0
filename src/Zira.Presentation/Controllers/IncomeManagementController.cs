1dd4e673161c8fb023c9e6577e030b684df26a89
KKDinev20 <KKDinev20@codingburgas.bg>
2025-01-05 19:07:18 +0200

    Create EditIncome method

--- src/Zira.Presentation/Controllers/IncomeManagementController.cs
+++ src/Zira.Presentation/Controllers/IncomeManagementController.cs
@@ -61,4 +61,64 @@ public class IncomeManagementController : Controller
 
         return this.View(incomes);
     }
+
+    public async Task<bool> DeleteIncome(Guid id)
+    {
+        var user = await this.context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return false;
+        }
+
+        this.context.Users.Remove(user);
+        await this.context.SaveChangesAsync();
+        return true;
+    }
+
+    [HttpGet("/edit-income/{id}")]
+    public async Task<IActionResult> EditIncome(Guid id)
+    {
+        var income = await this.context.Incomes
+            .FirstOrDefaultAsync(i => i.IncomeId == id && i.UserId == this.User.GetUserId());
+
+        if (income == null)
+        {
+            return this.NotFound();
+        }
+
+        this.ViewBag.Sources = Enum.GetValues(typeof(Sources)).Cast<Sources>().ToList();
+
+        return this.View(income);
+    }
+
+    [HttpPost("/edit-income/{id}")]
+    public async Task<IActionResult> EditIncome(Guid id, Income model)
+    {
+        if (id != model.IncomeId)
+        {
+            return this.BadRequest();
+        }
+
+        if (this.ModelState.IsValid)
+        {
+            var income = await this.context.Incomes
+                .FirstOrDefaultAsync(i => i.IncomeId == id && i.UserId == this.User.GetUserId());
+
+            if (income == null)
+            {
+                return this.NotFound();
+            }
+
+            income.Source = model.Source;
+            income.Amount = model.Amount;
+            income.DateReceived = model.DateReceived;
+
+            await this.context.SaveChangesAsync();
+
+            return this.RedirectToAction("IncomeList");
+        }
+
+        this.ViewBag.Sources = Enum.GetValues(typeof(Sources));
+        return this.View(model);
+    }
 }
