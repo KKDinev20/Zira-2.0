a465baaaa85b7b8d807bad96a59e83990272905f
KKDinev20 <KKDinev20@codingburgas.bg>
2025-02-02 18:29:30 +0200

    Create BudgetController and implement a monthly budget setup

---
+++ src/Zira.Presentation/Models/BudgetListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Zira.Data.Models;
+
+namespace Zira.Presentation.Models;
+
+public class BudgetListViewModel
+{
+    public List<Budget> Budgets { get; set; }
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+}
