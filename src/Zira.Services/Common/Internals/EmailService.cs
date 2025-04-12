a95decb43d850b8d96be451b764f4c9ad5e8be07
KKDinev20 <kkdinev20@codingburgas.bg>
2025-04-12 12:39:28 +0300

    Set offline option for sendgrid

--- src/Zira.Services/Common/Internals/EmailService.cs
+++ src/Zira.Services/Common/Internals/EmailService.cs
@@ -11,30 +11,39 @@ namespace Zira.Services.Common.Internals
     public class EmailService : IEmailService
     {
         private readonly string? apiKey;
-
+        private readonly bool emailEnabled;
+    
         public EmailService(IConfiguration configuration)
         {
             this.apiKey = configuration.GetValue<string>("SendGrid:ApiKey");
-            if (string.IsNullOrEmpty(this.apiKey))
+            this.emailEnabled = configuration.GetValue<bool>("EnableEmailSending");
+    
+            if (string.IsNullOrEmpty(this.apiKey) && emailEnabled)
             {
                 throw new Exception("SendGrid API key is not configured.");
             }
         }
-
+    
         public async Task SendEmailAsync(EmailModel emailModel)
         {
+            if (!emailEnabled)
+            {
+                Console.WriteLine("Email sending disabled (dev/offline mode).");
+                return;
+            }
+    
             try
             {
                 var client = new SendGridClient(this.apiKey);
                 var from = new EmailAddress("kkdinev20@codingburgas.bg");
                 var to = new EmailAddress(emailModel.ToEmail);
                 var msg = MailHelper.CreateSingleEmail(from, to, emailModel.Subject, emailModel.Body, emailModel.Body);
-
+    
                 var response = await client.SendEmailAsync(msg);
-
+    
                 var responseBody = await response.Body.ReadAsStringAsync();
                 Console.WriteLine($@"SendGrid Response: {response.StatusCode}, {responseBody}");
-
+    
                 if (response.StatusCode != System.Net.HttpStatusCode.Accepted)
                 {
                     throw new Exception($"Error sending email. Status Code: {response.StatusCode}");
